e3221b79ad3c32a95ec068eb76e8cdecfc5e715c
exhan <69556083+exeptionhandler@users.noreply.github.com>
2025-07-08 16:41:02 -0500

    5th Push

---
+++ content/res_24488822_0/trackingConfig.js
@@ -0,0 +1,17 @@
+var TrackingConfig = {
+    "success_status": "passed",
+    "unsuccess_status": "failed",
+    "send_score": "true",
+    "send_status": "true",
+    "send_interactions": "true",
+    "send_objectives": "true",
+    "mastery_score": "80",
+    "type": "tincanGlobal",
+    "TinCanMasteryScore": "80",
+    "TinCanEndPoint": "",
+    "TinCanAuthUser": "IDUSER",
+    "TinCanPassword": "PASSWORDTINCAN",
+    "TinCanUserMail": "mailto:user@netex.es",
+    "TinCanUserName": "username",
+    "TinCanStateId": "courseData"
+}
